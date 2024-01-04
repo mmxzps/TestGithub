42351304bc938ef764d5f258f5f8f944914888c7
ordio <max@maxsamuelsson.se>
2024-01-04 11:17:24 +0100

    Added Max Class and with method

---
+++ TestGithub/MaxClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithub
+{
+    internal class MaxClass
+    {
+        public string Name { get; set; }
+
+        public void Method() 
+        {
+            Console.WriteLine("method");
+        }
+    }
+}
